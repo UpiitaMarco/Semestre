21f2e04741b047737e3f2dfea5e7044c4e45cc14
UpiitaMarco <42361680+UpiitaMarco@users.noreply.github.com>
2018-08-13 20:34:24 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h> //Inicio, esto es la configuracion de cabeceras
+#include <stdlib.h> 
+
+int main(int argc, char *argv[])
+{
+int a, b, c;
+  printf("Hola mundo \n"); //Este comando manda el mensaje
+  system("PAUSE");	//Este es para terminar
+  return 0; //Aqui acaba el programa
+}
+
+void otro
+{
+     printf ()
+}
